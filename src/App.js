b45559a1528276d64119ff5c573816e6ba93539f
Tomer Dore <tomerdore963@gmail.com>
2021-01-27 20:48:20 +0200

    Sidebar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from './Header';
 
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
+      <div className='app__page'>
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 };
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './RecommendedVideos.css';
+
+const RecommendedVideos = () => {
+  return <div className='recommendedVideos'></div>;
+};
+
+export default RecommendedVideos;
